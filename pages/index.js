6bbf383914f5ce4d017683902bb5e86bd705e2d4
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-08-26 20:28:47 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,77 @@
+import React, {useState, useEffect} from 'react'
 import Head from 'next/head'
+import axios from "axios"
+import GithubCorner from "../src/components/GithubCorner"
 
 export default function Home() {
+
+  const [quote, setQuote] = useState("A quote fica aqui");
+  const [image, setImage] = useState("A image fica aqui");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get("https://api.chucknorris.io/jokes/random")
+    .then((response) => {
+      setQuote(response.data.value);
+      setImage(response.data.icon_url);
+      setLoading(false)
+    })
+  }, [])
+
+  const anotherQuote = () => {
+    setQuote("Loading")
+    axios.get("https://api.chucknorris.io/jokes/random")
+    .then((response) => {
+      setQuote(response.data.value)
+    })
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <>
+    <GithubCorner />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Chuck Norris Api</title>
+        <link rel="icon" href={image} />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      {/* <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+          <h2 className="text-2xl font-bold">
+            Chuck Norris random Quotes
+          </h2>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      </main> */}
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="min-w-screen items-center justify-center px-5 py-5">
+            <div className="w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800" style={{maxWidth: "500px"}} >
+                <div className="w-full pt-1 pb-5">
+                    <div className="overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg">
+                        <img src="https://pbs.twimg.com/profile_images/1254509037594517504/3AerCb3D_400x400.jpg" alt="Chuck Norris" />
+                    </div>
+                </div>
+                <div className="w-full mb-10">
+                    <div className="text-3xl text-indigo-500 text-left leading-tight h-3">“</div>
+                    <p className="text-sm text-gray-600 text-center px-5">
+                      {loading ?
+                      "Loading"
+                      :
+                      quote
+                      }
+                    </p>
+                    <div className="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">”</div>
+                </div>
+                <div className="w-full">
+                    <p className="text-md text-indigo-500 font-bold text-center">Chuck Norris</p>
+                </div>
+            </div>
+            <button 
+                className="mx-auto my-4 block bg-indigo-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" 
+                onClick={anotherQuote}
+            >
+              Refresh
+            </button>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
+    </>
   )
 }
